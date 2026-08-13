5b18fd6f2b7ed0637f02a0732add10fcdd696836
Anastasiia Pokasova <pokasovaai@gmail.com>
2026-08-13 03:33:24 +0300

    add flights service preparation files

---
+++ flights/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE flight
+(
+    id              SERIAL PRIMARY KEY,
+    flight_number   VARCHAR(20)              NOT NULL,
+    datetime        TIMESTAMP WITH TIME ZONE NOT NULL,
+    from_airport_id INT REFERENCES airport (id),
+    to_airport_id   INT REFERENCES airport (id),
+    price           INT                      NOT NULL
+);
+
+CREATE TABLE airport
+(
+    id      SERIAL PRIMARY KEY,
+    iata    VARCHAR(3),
+    name    VARCHAR(255),
+    city    VARCHAR(255),
+    country VARCHAR(255)
+);
