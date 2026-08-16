fc50ddcd07ed1f64e1284a9067a459c8e213f398
Anastasiia Pokasova <pokasovaai@gmail.com>
2026-08-16 08:51:17 +0300

    add services

--- flights/cruds/flights.py
+++ flights/cruds/flights.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Type, Any
 
 from sqlalchemy.orm import Session
 
@@ -13,12 +13,12 @@ class FlightCrud:
             self,
             offset: int = 0,
             limit: int = 100,
-    ) -> List[Flight]:
+    ) -> list[Type[Flight]]:
         airports = self._db.query(Flight)
         return airports.offset(offset).limit(limit).all()
 
-    async def get_by_id(self, id: int) -> Flight | None:
-        return self._db.query(Flight).filter(Flight.id == id).first()
+    async def get_by_id(self, flight_id: int) -> Flight | None:
+        return self._db.query(Flight).filter(Flight.id == flight_id).first()
 
     async def create(self, airport: Flight) -> Flight | None:
         try:
@@ -29,8 +29,29 @@ class FlightCrud:
             self._db.rollback()
 
         return airport
+    
+    async def update(self, airport: Flight, update_fields: dict[str, Any]) -> Flight | None:
+        for key, value in update_fields.items():
+            setattr(airport, key, value)
+
+        try:
+            self._db.add(airport)
+            self._db.commit()
+            self._db.refresh(airport)
+        except Exception:
+            self._db.rollback()
+            return None
+
+        return airport
 
     async def delete(self, airport: Flight) -> None:
         self._db.delete(airport)
         self._db.commit()
 
+    async def filter(self, filters: dict[str, Any]) -> list[Type[Flight]]:
+        conditions = [
+            getattr(Flight, column) == value
+            for column, value in filters.items()
+            if value
+        ]
+        return self._db.query(Flight).filter(*conditions).all()
